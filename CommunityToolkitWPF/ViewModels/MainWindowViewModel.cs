51c45cf063a1660f0924483863813512d9652706
arslanmurat06 <mrtarsln06@gmail.com>
2022-09-22 14:17:42 +0300

    net 6 wpf app created with community tool kit

---
+++ CommunityToolkitWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CommunityToolkitWPF.ViewModels
+{
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+
+        [ObservableProperty]
+        public string? firstName = "Kemal";
+
+
+        [RelayCommand]
+        public void ClickMe()
+        {
+            FirstName = "Hasan";
+        }
+    } 
+}
